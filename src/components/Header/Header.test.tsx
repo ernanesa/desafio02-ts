b7f964d887b93b2a07fb59782fc9abc08adb9507
Ernane Sa <ernane.sa@gmail.com>
2026-08-28 16:37:53 -0300

    style: formata teste do Header

--- src/components/Header/Header.test.tsx
+++ src/components/Header/Header.test.tsx
@@ -2,19 +2,18 @@ import { ChakraProvider } from "@chakra-ui/react";
 import { render, screen } from "@testing-library/react";
 import { describe, it, expect } from "vitest";
 import { Header } from "./Header";
-import system from '../../theme';
+import system from "../../theme";
 
 describe("Header", () => {
-    it("renderiza o link com o nome acessível 'Dio Bank - página inicial'", () => {
-        render(
-            <ChakraProvider value={system}>
-                <Header />
-            </ChakraProvider>
-        );
+  it("renderiza o link com o nome acessível 'Dio Bank - página inicial'", () => {
+    render(
+      <ChakraProvider value={system}>
+        <Header />
+      </ChakraProvider>,
+    );
 
+    const link = screen.getByRole("link", { name: "Dio Bank - página inicial" });
 
-        const link = screen.getByRole("link", { name: "Dio Bank - página inicial" });
-
-        expect(link).toBeInTheDocument();
-    });
+    expect(link).toBeInTheDocument();
+  });
 });
